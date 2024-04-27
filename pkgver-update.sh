8bc0ec2e10e4213e5ec354e1144c1d57af4c1592
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-04-27 13:05:38 +0800

    ci: auto update

--- pkgver-update.sh
+++ pkgver-update.sh
@@ -5,10 +5,11 @@ PKGBUILD=pkgbuilds/telegram-desktop-sakari/PKGBUILD
 source $PKGBUILD
 
 latest_tag=$(curl -s "https://api.github.com/repos/telegramdesktop/tdesktop/tags" | jq -r '.[0].name')
+newver=${latest_tag/v/}
 
-if [ "${latest_tag/v/}" != "${pkgver}" ]
+if [ "${newver}" != "${pkgver}" ]
 then
-    sed -i "s/pkgver=$pkgver/pkgver=${latest_tag/v/}/" $PKGBUILD
+    sed -i "s/pkgver=$pkgver/pkgver=${newver}/" $PKGBUILD
     if [ "${pkgrel}" != 1 ]
     then
         sed -i "s/pkgrel=$pkgrel/pkgrel=1/" $PKGBUILD
@@ -21,10 +22,11 @@ then
     newsha=$(sha512sum tdesktop.tar.gz)
     sed -i "s/${oldsha}/${newsha}/" $PKGBUILD
 
-    echo "Updated!"
+    echo "Updated from ${pkgver} to ${newver}!"
 
     # echo 1 if updated
     echo 1 >> updated
+    echo ${newver} >> newver
     exit 0
 fi
 echo "No Update!"
